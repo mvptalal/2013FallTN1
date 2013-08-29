0eb9897b700f601539bbfc981ef93def876a0494
unknown <n02176156@.active.newpaltz.edu>
2013-08-29 16:18:15 -0400

    Twiter Bootstrap

---
+++ Playground/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<style>
+  		
+  		.row div {
+  			
+  			background-color: #AACCCC;
+  		}
+  	</style>
+    <h1>This is the play ground</h1>
+		<div class "container">
+
+			<div class="row">
+		 	 <div class="col-xs-12 col-md-8">.col-xs-12 col-md-8</div>
+		  	<div class="col-xs-6 col-md-4">.col-xs-6 .csol-md-4</div>
+			</div>
+		
+		<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+			<div class="row">
+		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+			</div>
+		
+		<!-- Columns are always 50% wide, on mobile and desktop -->
+			<div class="row">
+		  <div class="col-xs-6">.col-xs-6</div>
+		  <div class="col-xs-6">.col-xs-6</div>
+			</div>
+
+		</div>
+
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
