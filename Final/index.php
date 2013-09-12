56a92f6d15ad1d61d3445d0947f5d1852f3fefdf
unknown <Talal@.(none)>
2013-09-12 14:08:20 -0400

    proj1

--- Final/index.php
+++ Final/index.php
@@ -5,16 +5,94 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
+	
   </head>
+  
   <body>
-    <h1>THis is the Final</h1>
+  <nav class="navbar navbar-inverse" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Playground</a>
+  </div>
 
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Links</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li><a href="#">Separated link</a></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+    
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Signed in as Talal Nweiran</a></li>
+     
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+
+<div class="jumbotron">
+  <div class="container">
+    <h1>Hello, world!</h1>
+    <p>Fall 2013 Web Server Programing</p>
+    <p><a class="btn btn-success btn-lg">Learn more</a></p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Heading</h3>
+        <p>This is a Header</p>
+        <p><a href="#" class="btn btn-primary">Veiw details>></a>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Important Points</h3>
+        <p>three main links</p>
+        <p><a href="#" class="btn btn-primary">Veiw details>></a>
+      </div>
+    </div>
+  </div>
+
+<div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Heading</h3>
+        <p>the first heading</p>
+        <p><a href="#" class="btn btn-primary">Veiw details>></a>
+      </div>
+    </div>
+  </div>
+
+
+
+
+</div>
+  
+ </div> 
     <script src="//code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
   </body>
