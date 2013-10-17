3d706c27a3f0b9cea07091a710cb5da93da5d1f9
unknown <n02176156@.active.newpaltz.edu>
2013-10-17 14:32:46 -0400

    midterm

--- Final/inc/_global.php
+++ Final/inc/_global.php
@@ -3,6 +3,20 @@ include_once ('_password.php');
 
 include_once __DIR__ . '../Models/Keywords.php';
 include_once __DIR__ . '../Models/Users.php';
+include_once __DIR__ . '../Models/Addresses.php';
+include_once __DIR__ . '../Models/ContactMethods.php';
+include_once __DIR__ . '../Models/Orders.php';
+include_once __DIR__ . '../Models/Order_Items.php';
+include_once __DIR__ . '../Models/Product_Categories.php';
+include_once __DIR__ . '../Models/Supliers.php';
+include_once __DIR__ . '../Models/Products.php';
+
+
+
+
+
+
+
 
 function GetConnection()
 {
---
+++ Final/inc/_password.php
@@ -0,0 +1,7 @@
+<?php
+function GetConnection()
+{
+	global $sql_password;
+	$sql_password = 's928649';
+	return;
+}
