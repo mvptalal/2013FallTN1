31bf53a9ac670d164c0e62d13030eacdcc900890
unknown <n02176156@.active.newpaltz.edu>
2013-11-11 16:17:46 -0500

    functions

--- Final/Views/Users/index.php
+++ Final/Views/Users/index.php
@@ -63,6 +63,10 @@ switch ($format) {
 	case 'dialog':
 		include '../Shared/_DialogLayout.php';
 		break;
+		
+	case "plain":
+	include $view;
+		break;
 	
 	default:
 		include '../Shared/_Layout.php';
--- Final/Views/Users/list.php
+++ Final/Views/Users/list.php
@@ -1,20 +1,26 @@
 <link href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/css/jquery.dataTables.min.css" type="text/css" rel="stylesheet" />
 <style>
-	tr.success, tr.success td{
-		background-color: #00AA00;
-	}
+        .table tr.success2, .table tr.success2 td{
+                background-color: #FFAA00 !important;
+        }
+        #table-wrapper{
+        	transition: width .5s;
+            -webkit-transition: width .5s; 
+        }
 </style>
 <div class="container">
         
         <h2>Users</h2>
         
-        <? if(isset( $_REQUEST['status']) && ($_REQUEST['status'] == 'Saved'): ?>
-        <div class="alert alert-success"> </div>
-        <b> Success!</b> You're user has been Saved.
-        <? endif ?>
+        <? if(isset($_REQUEST['status']) && $_REQUEST['status'] == 'Saved'): ?>
+                <div class="alert alert-success">
+                        <button type="button" class="close" aria-hidden="true">&times;</button>
+                        <b>Success!</b> You're User has been saved.
+                </div>
+        <? endif; ?>
         
         <a href="?action=new">Add Contact</a>
-        
+        <div id="table-wrapper" class="col-md-12">
         <table class="table table-hover table-bordered table-striped">
                 <thead>
                 <tr>
@@ -26,32 +32,63 @@
                 </thead>
                 <tbody>
                 <? foreach ($model as $rs): ?>
-                        <tr class" <?$model['id']==$_REQUEST['id'] ? /* <-means then terinary operator*/ 'success' : /*<-if not */ '' ?>">
+                        <tr class=" <?= $rs['id']==$_REQUEST['id'] ? 'success' : '' ?> ">
                                 <td><?=$rs['FirstName']?></td>
                                 <td><?=$rs['LastName']?></td>
                                 <td><?=$rs['UserType_Name']?></td>
                                 <td>
-                                        <a class="glyphicon glyphicon-file" href="?action=details&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="myModal"></a>
-                                        <a class="glyphicon glyphicon-pencil" href="?action=edit&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="myModal"></a>
-                                        <a class="glyphicon glyphicon-trash" href="?action=delete&id=<?=$rs['id']?> &format=dialog" data-toggle="modal" data-target="myModal"></a>
+                                        <a class="glyphicon glyphicon-file" href="?action=details&id=<?=$rs['id']?>"></a>
+                                        <a class="glyphicon glyphicon-pencil" href="?action=edit&id=<?=$rs['id']?>"></a>
+                                        <a class="glyphicon glyphicon-trash" href="?action=delete&id=<?=$rs['id']?>"></a>
                                 </td>
                         </tr>
                 <? endforeach ?>
                 </tbody>
         </table>
+       </div>
+       <div id="details" class="col-md-6"></div>
 </div>
 
-<div id="myModal" class="modal fade"></div>
+<div id="myModal" class="modal fade">
+	
+</div>
 
+</div>
 <? function Scripts(){ ?>
         <script src="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js"></script>
         <script type="text/javascript">
         $(function(){
-        	  $(".table").dataTable();
-        	$(".alert .close").click(function(){
-        		$(this).closest(".alert").slideUp();
-        	});
+                $(".table").dataTable();
+                $(".alert .close").click(function(){
+                        $(this).closest(".alert").slideUp();
+                });
+                
+                $(".table tr").click(function(){
+                	
+                	
+                	if($(this).closest("tr").hasClass("success2")){
+                	$(".success2").removeClass("success2");            
+                	$("#table-wrapper").removeClass("col-md-6").addClass("col-md-12"); 
+                	$("#details").html(''):
+
+                		
+                	}else{
+                     $(this.closest("tr").addClass("success2");  
+                     $(".success2").removeClass("success2");  
+                     $("#table-wrapper").removeClass("col-md-12").addClass("col-md-6"); 
+					 $("#details").load(this.href, {format: "plain"}, function(){
+					 	$("#details form").submit(HandelSubmit);
+					 });
+					 
+                	}  	
+                  
+                	return false;
+                });
+                
+                var HandleSubmit = function (){
+                	$("#details").html(JSON.stringify($(this).serializeArray()));
+                	return false;
+                }
         })
-              
         </script>
 <? } ?>
